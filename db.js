c4ef0815add5727d312cee2484fb095d65ee720d
solanamr <romerosolana@gmail.com>
2023-01-09 15:26:06 -0300

    trying to deploy with railway

--- db.js
+++ db.js
@@ -1,10 +1,26 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = process.env;
+
+// const CONEXION_DATA =
+//   process.env.DATABASE_URL ||
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+// const sequelize = new Sequelize(CONEXION_DATA, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
 const CONEXION_DATA =
   process.env.DATABASE_URL ||
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  DB_DEPLOY
+
 const sequelize = new Sequelize(CONEXION_DATA, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -15,5 +31,7 @@ const sequelize = new Sequelize(CONEXION_DATA, {
     },
   },
 });
+
+
 
 module.exports = sequelize;
